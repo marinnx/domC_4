6c6ed82b17b8e46e642b51f12078b0ad8a14c63a
marinnx <marinnx@mail.ru>
2023-07-22 11:03:04 +0900

    Добавлен код для формирования массива из 8 случайных чисел

---
+++ task29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GenerateArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < 7; i++)
+    {
+        System.Console.Write($"{array[i]}, ");        
+    }
+    System.Console.Write($"{array[7]}");
+    System.Console.Write("]");
+}
+
+int[] array = GenerateArray(8, 1, 99);
+PrintArray(array);
